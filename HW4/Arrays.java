044cc817e2244e3886dc7aa6f8f7499fab0d0626
Tori Hogrebe <90363135+thogrebe@users.noreply.github.com>
2021-12-14 20:54:51 -0500

    Update Arrays.java

--- HW4/Arrays.java
+++ HW4/Arrays.java
@@ -1,4 +1,5 @@
 package HW4;
+import java.util.Scanner;
 
 public class Arrays {
 
@@ -23,7 +24,24 @@ public class Arrays {
     // Total: 22
     // Array: {12,2,3,4,1,0}
     public static void Arraysums(){
+        Scanner input = new Scanner(System.in);
 
+        //The array, can change 10 to whatever size is necessary, assumed total is 10
+        int[] inputs = new int[10]; 
+
+        System.out.println("Please enter 10 numbers:");
+        for (int i = 0; i< 10; i++){
+            inputs[i] = input.nextInt();
+
+        }
+        for (int i = 1; i<10; i++){
+
+            inputs[i] = inputs[i] + inputs[i - 1];
+        }
+        for(int i = 0; i<10; i++) 
+        System.out.println("The sum at this point is: " + inputs[i]);
+
+        input.close();
     }
 
     // Create a method that will brute force a password EX.
@@ -46,7 +64,19 @@ public class Arrays {
         char[] alphabet = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
         '!', '@', '#', '$', '%', '^', '&', '*', '(', ')','<','>','/','?'};
-    
+        
+        char[] passwords = password.toCharArray();
+
+        for(int i = 0; i< passwords.length; i++){
+
+            for (int j = 0; j< alphabet.length; j++){
+                if( passwords[i] == alphabet[j]){
+                    answer+= alphabet[j];
+                    break;
+                }
+            }
+            System.out.println(answer);
+        }
         return answer; 
     }
 
@@ -59,7 +89,18 @@ public class Arrays {
     // Hint 2: 2 nested For loops should be all thats needed for the swapping logic
     public static int[] sorter(int[] nums){
         int [] sortedArray = new int[nums.length];
-
+            for( int i = 0;i< nums.length; i++) {
+                sortedArray[i] = nums[i];
+            }
+            for(int i = 0; i< sortedArray.length; i++){
+                    for(int j = 1; j< sortedArray.length; j++ ){
+                        if( sortedArray[j - 1] > sortedArray[j]){
+                            int sub = sortedArray[j - 1];
+                            sortedArray[j - 1] = sortedArray[j];
+                            sortedArray[j] = sub;
+                        }
+                    }
+            }
         return sortedArray;
     }
 
@@ -67,6 +108,15 @@ public class Arrays {
 
 
     public static void main(String[] args) {
+         System.out.println("Testing the Array Sum Code");
+         Arraysums();
+
+        System.out.println("Testing the Brute force");
+         bruteForce("AB!?");
+
+         System.out.println("Testing the Array Sorter 3000");
+         
+         System.out.println(java.util.Arrays.toString(sorter(new int[]{4,3,2,5,6})));
 
     }
 
